2f6c70adaefb0a9304c5c4d976dd516a728c1de8
HughEvans01 <hughevaans01@outlook.com>
2021-11-03 17:37:51 +0000

    Added automatic platform generation

--- game.js
+++ game.js
@@ -17,6 +17,7 @@ var config = {
 };
 
 var player;
+var maxPlatforms = 25;
 
 var game = new Phaser.Game(config);
 
@@ -32,13 +33,13 @@ function create () {
     //  A simple background for our game
     this.add.image(400, 300, 'sky');
     
-    this.movingPlatforms = this.physics.add.group({
+    this.platforms = this.physics.add.group({
         allowGravity: false,
         immovable: true,
     });
 
-    this.movingPlatforms.create(400, 400, 'ground');
-    this.movingPlatforms.create(1000, 400, 'ground');
+    this.platforms.create(400, 400, 'ground');
+    this.platforms.create(1000, 400, 'ground');
 
     // The player and its settings
     player = this.physics.add.sprite(400, 300, 'dude');
@@ -71,24 +72,33 @@ function create () {
     //  Input Events
     cursors = this.input.keyboard.createCursorKeys();
 
-    this.physics.add.collider(player, this.movingPlatforms);
+    //this.physics.add.collider(player, this.platforms);
+
+    this.physics.add.collider(
+        player,
+        this.platforms,
+        function (_player, _platform) {
+            if (_player.body.touching.up && _platform.body.touching.down) {
+                _player.body.velocity.y = 0;
+            }
+        });
 }
 
 function update () {
     // Scroll level / move player left and right
     if (cursors.left.isDown) {
        //player.setVelocityX(-160);
-       this.movingPlatforms.setVelocityX(300);
+       this.platforms.setVelocityX(300);
 
         player.anims.play('left', true);
     } else if (cursors.right.isDown) {
        //player.setVelocityX(160);
-        this.movingPlatforms.setVelocityX(-300,);
+        this.platforms.setVelocityX(-300,);
 
         player.anims.play('right', true);
     } else {
         player.setVelocityX(0);
-        this.movingPlatforms.setVelocityX(0);
+        this.platforms.setVelocityX(0);
 
         player.anims.play('turn');
     }
@@ -99,5 +109,26 @@ function update () {
     // Kill player if they fall out of the level
     if (player.y > 600) {
         this.scene.restart();
+    }
+    // Spawn a new platform
+    var totalPlatforms = this.platforms.children.entries.length;
+    latestPlatform = this.platforms.children.entries[totalPlatforms-1];
+    if (latestPlatform.x == 400) {
+        // Difference in y from height of prev platform
+        var value;
+        var maxHeight = 200;
+        var minHeight = 450;
+        if (latestPlatform.y >= maxHeight && latestPlatform.y <= minHeight) {
+            value = Phaser.Math.Between(-1, 1) * 50;
+        } else if (latestPlatform.y <= maxHeight) {
+            value = 50;
+        } else if (latestPlatform.y >= minHeight) {
+            value = -50;
+        }
+        this.platforms.create(1000, latestPlatform.y + value, 'ground');
+        // Destroy platforms that are a long way behind the player
+        if (totalPlatforms > maxPlatforms) {
+            this.platforms.children.entries[0].destroy();
+        }
     }
 }
