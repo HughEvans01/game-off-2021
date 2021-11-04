55179d1c4c47668f5eb979aded075b1cdfa5fb7a
HughEvans01 <hughevaans01@outlook.com>
2021-11-04 18:39:26 +0000

    Added mobile support

--- game.js
+++ game.js
@@ -2,6 +2,10 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    scale: {
+        mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT ,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
     physics: {
         default: 'arcade',
         arcade: {
@@ -18,14 +22,31 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-var player;
-var maxObjects = 25;
+
+
+//TODO Define as many game attributes up here as possible
+var player; // Stores reference to player sprite
+var maxObjects = 25; // Maximum number of each type of object
+var speed = 300; // Player speed
+var jump = 330; // Jump velocity
+var maxHeight = 200; // Max height for platforms to spawn
+var minHeight = 450; // Min height for platforms to spawn
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    var mobile = true;
+} else {
+    var mobile = false;
+}
+// TODO Find width and height of game and substitute for hard coded values
+// var width = this.cameras.main.centerX, this.cameras.main.centerY
 
 function preload () {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
+    this.load.image('left', 'assets/left.png');
+    this.load.image('up', 'assets/up.png');
+    this.load.image('right', 'assets/right.png');
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
@@ -83,6 +104,22 @@ function create () {
     //  Input Events
     cursors = this.input.keyboard.createCursorKeys();
 
+    // Touch screen controls
+    if (mobile) {
+        this.input.addPointer(2); // For multi-touch
+        const leftButton = this.add.image(48, 549-95, 'left');
+        const upButton = this.add.image(139, 549-95, 'up');
+        const rightButton = this.add.image(230, 549-95, 'right');
+        leftButton.setInteractive();
+        upButton.setInteractive();
+        rightButton.setInteractive();
+        leftButton.on('pointerdown', () => { this.left_held = true; });
+        leftButton.on('pointerup', () => { this.left_held = false; });
+        upButton.on('pointerdown', () => { this.jump_pressed = true; });
+        rightButton.on('pointerdown', () => { this.right_held = true; });
+        rightButton.on('pointerup', () => { this.right_held = false; });
+    }
+
     this.physics.add.collider(player, this.platforms);
     this.physics.add.overlap(player, this.pickups, collectPickup, null, this);
     this.physics.add.overlap(player, this.enemies, hitEnemy, null, this);
@@ -90,18 +127,18 @@ function create () {
 
 function update () {
     // Scroll level / move player left and right
-    if (cursors.left.isDown) {
+    if (cursors.left.isDown || this.left_held) {
        //player.setVelocityX(-160);
-       this.platforms.setVelocityX(300);
-       this.pickups.setVelocityX(300);
-       this.enemies.setVelocityX(300);
+       this.platforms.setVelocityX(speed);
+       this.pickups.setVelocityX(speed);
+       this.enemies.setVelocityX(speed);
 
         player.anims.play('left', true);
-    } else if (cursors.right.isDown) {
+    } else if (cursors.right.isDown || this.right_held) {
        //player.setVelocityX(160);
-        this.platforms.setVelocityX(-300,);
-        this.pickups.setVelocityX(-300);
-        this.enemies.setVelocityX(-300);
+        this.platforms.setVelocityX(-speed,);
+        this.pickups.setVelocityX(-speed);
+        this.enemies.setVelocityX(-speed);
 
         player.anims.play('right', true);
     } else {
@@ -110,11 +147,15 @@ function update () {
         this.pickups.setVelocityX(0);
         this.enemies.setVelocityX(0);
 
+        this.left_pressed = false;
+        this.right_pressed = false;
+
         player.anims.play('turn');
     }
     // Jump
-    if (cursors.up.isDown && player.body.touching.down) {
-        player.setVelocityY(-330);
+    if ((cursors.up.isDown || this.jump_pressed) && player.body.touching.down) {
+        player.setVelocityY(-jump);
+        this.jump_pressed = false;
     }
     // Kill player if they fall out of the level
     if (player.y > 600) {
@@ -124,7 +165,7 @@ function update () {
     var totalPlatforms = this.platforms.children.entries.length;
     var latestPlatform = this.platforms.children.entries[totalPlatforms-1];
     // TODO Logic for spawning platforms at intervals is stupid, improve it
-    if (latestPlatform.x == 400) {
+    if (latestPlatform.x <= 400) {
         latestPlatform = spawnPlatform(this.platforms,latestPlatform);
         switch (Phaser.Math.Between(0, 2)) {
             case 0:
@@ -140,8 +181,6 @@ function update () {
 function spawnPlatform(platforms,latestPlatform) {
     // Difference in y from height of prev platform
     var value;
-    var maxHeight = 200;
-    var minHeight = 450;
     if (latestPlatform.y >= maxHeight && latestPlatform.y <= minHeight) {
         value = Phaser.Math.Between(-1, 1) * 50;
     } else if (latestPlatform.y <= maxHeight) {
@@ -177,7 +216,6 @@ function spawnEnemy(enemies,platform) {
     var totalEnemies = enemies.children.entries.length;
     // Destroy enemies that are a long way behind the player
     if (totalEnemies > maxObjects) {
-    console.log("test");
       enemies.children.entries[0].destroy();
     }
 }
