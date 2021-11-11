64e84c361316f8dc683339dc1662b499ce034d1b
HughEvans01 <hughevaans01@outlook.com>
2021-11-11 21:31:53 +0000

    Tweaked bug collection

--- bugCollection.js
+++ bugCollection.js
@@ -9,6 +9,7 @@ class BugCollection extends Phaser.Scene {
     }
 
     preload() {
+        this.load.image('sky', 'assets/sky.png');
         this.load.image('star', 'assets/star.png');
         this.load.json('bugs', 'bugs.json');
     }
@@ -18,6 +19,8 @@ class BugCollection extends Phaser.Scene {
             allowGravity: false,
             immovable: true,
         });
+
+        this.background = this.add.image(400, 300, 'sky');
 
         this.bugsJSON = this.cache.json.get('bugs');
         var gameStart = this.add.text(725, 575, 'BACK', { fontFamily: 'font1', fontSize: '32px' });
@@ -29,21 +32,25 @@ class BugCollection extends Phaser.Scene {
 
         // Display all bugs in a grid with collected bugs in colour
         this.bugCollection = JSON.parse(window.localStorage.getItem('bugCollection'));
-        console.log(this.bugCollection);
-        var x = 50;
+        var x = 100;
         var y = 50;
         var bug = null;
         for (var i=0; i<this.bugsJSON.length; i++) {
             bug = this.bugsJSON[i];
             var pickup = this.pickups.create(x, y, 'star');
-            if (this.bugCollection.includes(i)) {
-                pickup.setTintFill(bug.colour);
-            } else {
-                pickup.setTintFill("0x959aa1");
+            pickup.setOrigin(0.5,0.5);
+            pickup.setTintFill("0x959aa1");
+            if (this.bugCollection) {
+                if (this.bugCollection.includes(i)) {
+                    pickup.setTintFill(bug.colour);
+                    var text = this.add.text(x, y+25, bug.name, { fontFamily: 'font2', fontSize: '16px' });
+                    text.setOrigin(0.5,0.5);
+                }
             }
-            x = x + 50;
-            if (x > 300) {
-                x = 50;
+
+            x = x + 200;
+            if (x > 500) {
+                x = 100;
                 y = y + 50;
             }
         }
