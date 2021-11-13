bb99ffdff00d9bdb8b10f51f04715e2aacb3c9d7
HughEvans01 <hughevaans01@outlook.com>
2021-11-13 13:24:20 +0000

    Fixed issue where game started before fonts were loaded

--- config.js
+++ config.js
@@ -1,19 +1,22 @@
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scale: {
-        mode: Phaser.Scale.HEIGHT_CONTROLS_WIDTH,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 600 },
-            debug: false
-        }
-    },
-    scene: [ Menu, Game, BugCollection ]
-};
+// Ensure fonts are loaded before starting game
+document.fonts.ready.then(function(font_face_set) {
+    const config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        scale: {
+            mode: Phaser.Scale.HEIGHT_CONTROLS_WIDTH,
+            autoCenter: Phaser.Scale.CENTER_BOTH
+        },
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 600 },
+                debug: false
+            }
+        },
+        scene: [ Menu, Game, BugCollection ]
+    };
 
-new Phaser.Game(config);
+    new Phaser.Game(config);
+});
